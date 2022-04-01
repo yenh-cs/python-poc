de7cfc7e5477ef0a5f1dc4b26a76fdf2ebc7ace0
yenh-cs <yen.hoang@contentsquare.com>
2022-04-01 12:51:07 +0200

    Init project

---
+++ exercise1.py
@@ -0,0 +1,45 @@
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+class TestTemplate(unittest.TestCase):
+    def setUp(self):
+        self.target = "http://main.ctqatest.info/test.php"
+        
+        """Start web driver"""
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument("--window-size=1920,1080")
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+        self.driver.implicitly_wait(10)
+
+    def tearDown(self):
+        """Stop web driver"""
+        self.driver.quit()
+
+    def test_case_1(self):
+        """Find and click top-left logo button"""
+        try:
+            self.driver.get(self.target)
+            shadow_host = self.driver.find_element(By.CSS_SELECTOR, '#site-info')
+            shadow_root = shadow_host.shadow_root
+            shadow_content = shadow_root.find_element(By.CSS_SELECTOR, 'span')
+            assert shadow_content.text == 'This is a demo website for testing purpose'
+        except NoSuchElementException as ex:
+            self.fail(ex.msg)
+
+    
+    #Use the website http://main.ctqatest.info/test.php  and perform the following tasks.
+    #Go to the login page and enter the username as test@test.com and password as ThisIs@T3st. Check if there is an error displayed.
+    def test_login(self):
+        return None
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestTemplate)
+    unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ exercise2.py
@@ -0,0 +1,41 @@
+#	 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#	 
+#	 You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#	 
+#	 You can return the answer in any order.
+#	 
+#	  
+#	 
+#	 Example 1:
+#	 
+#	 Input: nums = [2,7,11,15], target = 9
+#	 Output: [0,1]
+#	 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+#	 Example 2:
+#	 
+#	 Input: nums = [3,2,4], target = 6
+#	 Output: [1,2]
+#	 Example 3:
+#	 
+#	 Input: nums = [3,3], target = 6
+#	 Output: [0,1]
+#	  
+#	 
+#	 Constraints:
+#	 
+#	 2 <= nums.length <= 104
+#	 -109 <= nums[i] <= 109
+#	 -109 <= target <= 109
+#	 Only one valid answer exists.
+def solution(nums):
+    print(nums)
+
+
+def main():
+    nums = [3, 2, 4]
+    answer = solution(nums)
+    print('The answer is {} and {}'.format(answer[0], answer[1]))
+
+
+if __name__ == '__main__':
+    main()
